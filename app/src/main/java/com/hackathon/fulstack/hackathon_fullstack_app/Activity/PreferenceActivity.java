1b5eb43b4a0f41fa9e35e76932342a6f120a71b9
Pratyush Singh <singh.pratyush96@gmail.com>
2016-03-14 14:24:34 +0530

    Renamed unconventionally named file by other contributor

--- app/src/main/java/com/hackathon/fulstack/hackathon_fullstack_app/Activity/PreferenceActivity.java
+++ app/src/main/java/com/hackathon/fulstack/hackathon_fullstack_app/Activity/PreferenceActivity.java
@@ -10,13 +10,12 @@ import android.view.View;
 
 import com.hackathon.fulstack.hackathon_fullstack_app.Manager.DatabaseManager;
 import com.hackathon.fulstack.hackathon_fullstack_app.Manager.RecyclerViewAdapter;
-import com.hackathon.fulstack.hackathon_fullstack_app.Models.Feed;
 import com.hackathon.fulstack.hackathon_fullstack_app.Models.Preference;
 import com.hackathon.fulstack.hackathon_fullstack_app.R;
 
 import java.util.ArrayList;
 
-public class add_preference extends AppCompatActivity {
+public class PreferenceActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
     LinearLayoutManager mLayoutManager;
@@ -31,22 +30,17 @@ public class add_preference extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //start
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerPrefs);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new RecyclerViewAdapter(getDataSet());
         mRecyclerView.setAdapter(mAdapter);
-
-        //end
-
-
     }
 
     protected void onResume() {
         super.onResume();
-        ((RecyclerViewAdapter) mAdapter).setOnItemClickListener(new RecyclerViewAdapter
+        mAdapter.setOnItemClickListener(new RecyclerViewAdapter
                 .MyClickListener() {
             @Override
             public void onItemClick(int position, View v) {
